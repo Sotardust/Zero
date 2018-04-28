173eb07e0984f2ce546be8ef9e3dec447b313993
daihaitao <daihaitao@wanwujieshu.net>
2018-04-28 11:40:49 +0800

    leakcanary

--- app/src/main/java/com/dai/zero/BaseActivity.java
+++ app/src/main/java/com/dai/zero/BaseActivity.java
@@ -2,7 +2,6 @@ package com.dai.zero;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import java.util.LinkedList;
@@ -18,27 +17,41 @@ public abstract class BaseActivity extends DaggerAppCompatActivity {
 
     private static final String TAG = "BaseActivity";
     // 管理运行的所有的activity
-    private final static List<AppCompatActivity> mActivities = new LinkedList<AppCompatActivity>();
+    private final static List<BaseActivity> mActivities = new LinkedList<BaseActivity>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         System.out.println("BaseActivity.onCreate");
 //        synchronized (mActivities) {
-//            mActivities.add(this);
+        mActivities.add(this);
 //        }
     }
 
     //添加Activity
-//    private void addActivity(AppCompatActivity activity) {
-//        mActivities.add(activity);
-//    }
+
+    private List<BaseActivity> getActivities() {
+        return mActivities;
+    }
+
+    private void deleteActivity(BaseActivity activity) {
+
+        System.out.println("activity = " + activity);
+        if (mActivities.contains(activity)) {
+            mActivities.remove(activity);
+        }
+    }
+
+    private void exit() {
+        mActivities.removeAll(mActivities);
+    }
 
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy: ");
+        deleteActivity(this);
         ZeroApplication.getInstance(getApplicationContext()).watch(this);
     }
 }
